c04a36fb849f31206b119571e15312e01446d2a2
t1h <t-hirano@mtj.biglobe.ne.jp>
2014-10-10 17:18:54 +0900

    重複した記述を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,10 @@ class User < ActiveRecord::Base
 
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.password = Devise.friendly_token[0,20]
-
       user.provider= auth.provider
       user.uid = auth.uid
       user.email = auth.uid + '@oauth_dummy.twitter.com' # 登録にemail必須のため
       user.password = Devise.friendly_token[0,20]
     end
   end
-
 end
